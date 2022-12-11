98307531eb97cb1bf12456664f50cfd515bada34
fahmi-mac <f.hidayah@pixilapps.com>
2022-12-11 21:48:57 +0700

    init commit

---
+++ wierd_algorithm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    long n;
+    cin >> n;
+
+    while(n > 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        }
+        else {
+            n = n * 3 + 1;
+        }
+        cout << n << "\n";
+    } 
+
+    return 0;
+}
