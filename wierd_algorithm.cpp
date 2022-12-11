9457375dc2e09fa8cb1815b2b0a3e6f7a419f27b
fahmi-mac <f.hidayah@pixilapps.com>
2022-12-11 21:53:54 +0700

    fixed wierd_algorithm

--- wierd_algorithm.cpp
+++ wierd_algorithm.cpp
@@ -1,20 +1,32 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+int main()
+{
     long n;
     cin >> n;
 
-    while(n > 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
+    while (n >= 1)
+    {
+        if (n == 1)
+        {
+            cout << n << "\n";
+            n = 0;
         }
-        else {
-            n = n * 3 + 1;
+        else
+        {
+            cout << n << "\n";
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else
+            {
+                n = n * 3 + 1;
+            }
         }
-        cout << n << "\n";
-    } 
+    }
 
     return 0;
 }
